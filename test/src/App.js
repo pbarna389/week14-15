f8fec1f6976d38fa126fb0f7954143bbf236329a
pbarna389 <64441402+pbarna389@users.noreply.github.com>
2022-06-29 11:17:24 +0200

    session end

--- test/src/App.js
+++ test/src/App.js
@@ -2,15 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import Timer from './components/Timer/timer';
 import Timer2 from './components/Timer2/timer2';
+import { useState } from 'react';
 
 
 function App() {
+  const [showTimer, setShowTimer] = useState(true);
+
+  // const state = useState(true);
+  // console.log(state);
+  // const showTimer = state[0];
+  // const setShow = state[1];
+
   return (
     <div className="App">
       <header className="App-header">
         {/* CLASS BASED COMPONENT */}
-        {/* <Timer /> */}
-        <Timer2 />
+        {showTimer ? <Timer /> : <div>No Timer for you!</div>}
+        <button onClick={() => showTimer ? setShowTimer(false) : setShowTimer(true)}>Hide</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
           Our communication has transcended to a new level.
--- test/src/components/Timer/timer.js
+++ test/src/components/Timer/timer.js
@@ -7,6 +7,20 @@ class Timer extends React.Component {
             time: 0
         }
         setInterval(() => { this.incrementTimer() }, 1000);
+
+        console.log("Constructor called!");
+    }
+
+    componentDidMount() {
+        console.log("Component created");
+    }
+
+    componentDidUpdate() {
+        console.log("Component updated!");
+    }
+
+    componentWillUnmount() {
+        console.log("Component unmounted")
     }
 
     incrementTimer() {
@@ -16,7 +30,7 @@ class Timer extends React.Component {
     }
 
     render() {
-        console.log("Render happened");
+        // console.log("render happened!")
         return (
             <>
                 <div>
